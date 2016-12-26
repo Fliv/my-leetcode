10b038bc4130270597f3f3a1aee9a7230816bc8d
Living <415071280@qq.com>
2016-12-26 20:32:27 +0800

    battleships-in-a-board

---
+++ battleships-in-a-board.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == 'X' and (i == 0 or board[i - 1][j] == ".") and (j == 0 or board[i][j - 1] == "."):
+                    count += 1
+        return count
+
+if __name__=="__main__":
+    solution = Solution()
+    # print solution.countBattleships([['x', '.', '.', 'x'], ['.', '.', '.', 'x'], ['.', '.', '.', 'x']])
+    print solution.countBattleships(["X..X", "...X", "...X"])
